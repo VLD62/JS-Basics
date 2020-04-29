09839663ab577f210556f5c81a0524a4f4c5f194
Vladislav Iliev <viliev2@dxc.com>
2020-04-29 22:11:09 +0300

    Add tasks from Conditinal statements lab.

---
+++ 03.Conditional-Statements/Lab/02. Greater Number.js
@@ -0,0 +1,9 @@
+function greaterNumber(num1, num2){
+    let number1 = Number(num1);
+    let number2 = Number(num2);
+    if (number1 > number2) {
+        console.log(number1);
+    } else {
+        console.log(number2);
+    }
+}
